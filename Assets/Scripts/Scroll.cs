47b6d4fa42767bab2ad44ca017116ebdef88845f
Josh8F <35751365+Josh8F@users.noreply.github.com>
2019-01-26 15:48:59 -0600

    Scroll update

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -4,34 +4,31 @@ using UnityEngine;
 
 public class Scroll : MonoBehaviour
 {
-    public Transform[] fondos;
-    public float[] velocidades;
-    public float smoothing = 1f;
-
-    private Transform camara;
-    private Vector3 posCamPrev;
+    public float velocidad = 5f;
+    private MeshRenderer renderer;
 
     private void Awake()
     {
-        camara = Camera.main.transform;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        posCamPrev = camara.position;
-        velocidades = new float[fondos.Length];
-        for (int i = 0; i < fondos.Length; i++) {
-            velocidades[i] = fondos[i].position.z * -1;
-        }
+        renderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < fondos.Length; i++) {
-            float parallax = (posCamPrev.x - camara.position.x) * velocidades[i];
-            float objetivo = fondos[i].position.x + parallax;
+        int direccion = 0;
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                direccion = -1;
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                direccion = 1;
+            }
+            renderer.material.mainTextureOffset = new Vector2(Time.time * velocidad * direccion, 0);
         }
+
     }
+
 }
