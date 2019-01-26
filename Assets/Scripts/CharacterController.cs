93d42025a6565ddc3eeb12672d44e17bd1ec12dc
David Carrera <brim.carrera@gmail.com>
2019-01-26 11:46:13 -0600

    Escena 01 y personaje
    Creación de la escena 01 y programación basica del movimiento del personaje.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public int velocidad = 5;
+    private SpriteRenderer spriteRenderer;
+
+    Rigidbody2D cuerpo;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+
+    void Start()
+    {
+        cuerpo = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movimiento = Vector2.zero;
+
+       float horizontal = Input.GetAxis("Horizontal");
+        movimiento.x = horizontal;
+
+        bool flipSprite = (spriteRenderer.flipX ? (movimiento.x > 0.01f) : (movimiento.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        cuerpo.velocity = Vector2.Lerp(cuerpo.velocity, movimiento, 1);
+    }
+}
