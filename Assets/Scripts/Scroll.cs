99cb83314695e3a1be780cf51b97ef4a6e04cb0e
Josh8F <35751365+Josh8F@users.noreply.github.com>
2019-01-26 12:37:49 -0600

    scroll incompleto

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public float velocidad = 0f;
+    public MeshRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset = new Vector2(Time.time * velocidad, 0);
+    }
+}
